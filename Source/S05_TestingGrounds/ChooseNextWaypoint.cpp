d8976ee0880cd72691e5d5c67c49e0337595b0f8
pv62 <pranav.v99@hotmail.com>
2019-03-02 15:28:44 +0000

    TG21 Composition over Inheritance

--- Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
@@ -3,17 +3,20 @@
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
-#include "PatrollingGuard.h"	// TODO remove coupling
+#include "PatrolRoute.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	// TODO Protect against empty patrol points
-
 	// Get the patrol points
-	auto AIController = OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; } // Protect against no patrol route
+
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	// Protect against empty patrol points
+	if (PatrolPoints.Num() == 0) { 
+		UE_LOG(LogTemp, Warning, TEXT("A guard is missing patrol points"));
+	}
 
 	// Set Next Waypoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
